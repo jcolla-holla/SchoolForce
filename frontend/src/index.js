c2c1de9dc924b8f9b43529ed0cf39a95a9712207
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-03-30 18:19:08 -0400

    removed most warnings

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,6 @@ import jwt_decode from "jwt-decode";
 import { setAuthToken } from "./util/session_api_util";
 import { logout } from "./actions/session_actions";
 
-import { createNewStudent } from "./actions/student_actions";
-import { createReminder } from "./actions/reminder_actions";
-import { signup } from "./actions/session_actions";
-
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
